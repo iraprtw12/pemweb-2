5a06e4a27af51746895e713911c73b97cf7a962e
iraprtw12 <irap22162ti@student.nurulfikri.ac.id>
2023-03-21 21:00:05 +0700

    praktikum

---
+++ P4_Ira_TI05/home.php
@@ -0,0 +1,38 @@
+<?php
+    include_once 'template/header.php';
+?>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nama Dosen</th>
+      <th scope="col">Mata Kuliah</th>
+      <th scope="col">Jam</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Ira Pratiwi</td>
+      <td>Basis Data</td>
+      <td>08.00 - 10.00</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Argya Arya Putri</td>
+      <td>Jaringan Komputer</td>
+      <td>10.00 - 12.00</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Hayatunnisa</td>
+      <td>Statistika & Probabilitas</td>
+      <td>13.00 - 15.00</td>
+    </tr>
+  </tbody>
+</table>
+
+<?php
+    include_once 'template/footer.php';
+?>
